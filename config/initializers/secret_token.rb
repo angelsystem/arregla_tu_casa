d041951bd2c5146e47bb16aa265011d4c076dbb7
angelito rojas <angelsystem1387@gmail.com>
2013-06-22 18:06:12 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArreglaTuCasa::Application.config.secret_token = '39a864bed569fd010edb845a99f6bef2d6ee103dd980817d956503ac0174774015ea9275ee7f2b6024c0877b23673aae85fce9ed9e146dc348e884a7bbf97353'
